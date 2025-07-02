1b70537ad7196def0593b1e4a40553e8f133c2bb
haseu12 <haseu12@naver.com>
2025-07-02 19:19:31 +0900

    teat

---
+++ spring_application/src/test/java/com/spring/mybatis/TestSqlSession.java
@@ -0,0 +1,33 @@
+package com.spring.mybatis;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.spring.dto.MemberVO;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:com/spring/context/application-context.xml")
+public class TestSqlSession {
+
+	@Autowired
+	private SqlSession session;
+	
+	@Test
+	public void testSelectMemberList()throws SQLException {
+		List<MemberVO> memberList
+		= session.selectList("Member-Mapper.selectMemberList");
+		
+		Assert.assertEquals(memberList.size(), 3);
+	}
+	
+	
+}
